dc26898f8425dfa23d5e75b6692b4e135f062564
ChysnTech <3140696291@qq.com>
2026-07-12 18:19:56 +0800

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "codeing" << endl;
+//int
+    int fs = 13;
+    if( fs < 100){
+        cout << "" << endl;
+    }
+while(true){
+std::cout << "hello!" << std::endl;
+}
+return 0;
+}
